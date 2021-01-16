ba4bbd84fac576590de4846e75ed2acacd58da97
YOUR NAME <YOUR EMAIL ADDRESS>
2021-01-16 15:49:03 -0800

    items added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,98 @@
 import "./App.css";
+import Header from "./components/HeaderFolder/Header";
+import Item from "./components/ItemsFolder/Item";
+import Accessories from "./assets/Desktop-Accessories.jpg";
+import ModelS from "./assets/Desktop-ModelS.jpeg";
+import Model3 from "./assets/Desktop-Model3.jpeg";
+import ModelX from "./assets/Desktop-ModelX.jpeg";
+import ModelY from "./assets/Desktop-ModelY.jpeg";
+import SolarPanels from "./assets/Desktop-SolarPanels.jpeg";
+import SolarRoof from "./assets/Desktop-SolarRoof.jpeg";
 
 function App() {
   return (
     <div className="app">
-      <h1>Tesla app</h1>
+      <Header />
+      <div className="app__itemsContainer">
+        <Item
+          title="Solar Panels"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={SolarPanels}
+          leftBtnTxt=""
+          leftBtnLink=""
+          rightBtnTxt=""
+          rightBtnLink=""
+          twoButtons="true"
+          first
+        />
+        <Item
+          title="Model S"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={ModelS}
+          leftBtnTxt=""
+          leftBtnLink=""
+          rightBtnTxt=""
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model 3"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={Model3}
+          leftBtnTxt=""
+          leftBtnLink=""
+          rightBtnTxt=""
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model X"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={ModelX}
+          leftBtnTxt=""
+          leftBtnLink=""
+          rightBtnTxt=""
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Model Y"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={ModelY}
+          leftBtnTxt=""
+          leftBtnLink=""
+          rightBtnTxt=""
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Solar for New Roofs"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={SolarRoof}
+          leftBtnTxt=""
+          leftBtnLink=""
+          rightBtnTxt=""
+          rightBtnLink=""
+          twoButtons="true"
+        />
+        <Item
+          title="Accessories"
+          desc="Money-back guarantee"
+          descLink=""
+          backgroundImg={Accessories}
+          leftBtnTxt=""
+          leftBtnLink=""
+          rightBtnTxt=""
+          rightBtnLink=""
+          twoButtons="true"
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/HeaderFolder/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./Header.css";
+import TeslaLogo from "../../assets/teslaLogoSmall.svg";
+import MenuIcon from "@material-ui/icons/Menu";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <img src={TeslaLogo} alt="Tesla Logo" />
+      </div>
+      <div className="header__center">
+        <p>Model S</p>
+        <p>Model 3</p>
+        <p>Model y</p>
+        <p>Solar Roof</p>
+        <p>Solar Panel</p>
+      </div>
+      <div className="header__right">
+        <p>Shop</p>
+        <p>Tesla Account</p>
+        <div className="header__burgerIcon">
+          <MenuIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
